65654ea6a08a9f30591f9229b9c9c60f2e504c12
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-03-02 19:19:57 -0300

    anatomia de um módulo angular

--- C8 - Modulos/modulo-proj/src/app/app.module.ts
+++ C8 - Modulos/modulo-proj/src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // aqui ficam todos os componentes, diretivas e pipes
   declarations: [
     AppComponent
   ],
+  // aqui pode importar outros módulos
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // aqui ficam o que eu quero fornecer para outros módulos
+  exports: [], // não vem por padrão nesse módulo principal, pois não faz sentido
+  providers: [], // aqui fica nossa injeção de dependência 
+  bootstrap: [AppComponent] // define o componente inicial
 })
 export class AppModule { }
