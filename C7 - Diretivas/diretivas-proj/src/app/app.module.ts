d559311a4d074131054b5b808f19b974ce487b79
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-03-02 11:40:16 -0300

    ngModel para formulário

--- C7 - Diretivas/diretivas-proj/src/app/app.module.ts
+++ C7 - Diretivas/diretivas-proj/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
 import { CompAtributosComponent } from './comp-atributos/comp-atributos.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { CompAtributosComponent } from './comp-atributos/comp-atributos.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- C7 - Diretivas/diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
+++ C7 - Diretivas/diretivas-proj/src/app/comp-atributos/comp-atributos.component.ts
@@ -10,8 +10,14 @@ export class CompAtributosComponent implements OnInit {
   estilo: string = 'disable'; // 'disable' é a nossa classe na CSS
   corFundo: string = 'blue';  // nosso CSS inline
   corDaFonte: string = 'red';  
+  item: string = ''; // ngModel
+  lista:string[] = [];
 
   constructor() { }
+
+  adicionarLista(){
+    this.lista.push(this.item);
+  }
 
   ngOnInit(): void {
   }
